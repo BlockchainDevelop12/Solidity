d1af11412ce6a81c60a38a5f2beadb3622f1be83
BlockchainDevelop12 <123594512+BlockchainDevelop12@users.noreply.github.com>
2023-01-25 21:54:39 +0100

    Add files via upload

---
+++ Lock.js
@@ -0,0 +1,12 @@
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+function setMessage(message) {
+  return message;
+}
+
+describe("setMessage", () => {
+  it("message", () => (){
+    const message = "hello world"
+  })
+}) 
---
+++ Lock.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.17;
+
+contract HelloWorld {
+
+    //string HelloWorld;
+
+    function hello() public pure returns(string memory) {
+        return "HelloWorld";
+    }
+}
